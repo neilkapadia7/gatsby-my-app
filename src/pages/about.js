e164323b4f1f57a7b4236773176be5fcbe12656b
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-09 14:51:33 +0530

    Initial Commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 const About = () =>  (
         <Layout>
         <div>
-            <h1>About Page</h1>
+            <h1>About Us Page</h1>
             <p>This is a sample site for my gatsby first app.</p>
         </div>
         </Layout>
